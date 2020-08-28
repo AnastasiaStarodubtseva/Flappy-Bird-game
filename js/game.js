7d48eb69298c70de1ed759125537c351669d8d23
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-08-28 14:49:31 +0300

    Creating a game background

---
+++ js/game.js
@@ -0,0 +1,25 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image(); // Создание объекта
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
+
+var gap = 90;
+
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(pipeUp, 100, 0);
+  ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+}
+
+pipeBottom.onload = draw;
