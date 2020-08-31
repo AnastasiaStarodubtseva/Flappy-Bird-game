949f9ec6337d2e2dff99078fff2f70b54ffef533
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-08-31 19:39:14 +0300

    Added sound effects, score, touch tracking, reload page

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
-var bird = new Image(); // Создание объекта
+var bird = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeUp = new Image();
@@ -13,16 +13,24 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png";
 pipeBottom.src = "img/pipeBottom.png";
 
+// Звуковые файлы
+var fly = new Audio();
+var score_audio = new Audio();
+
+fly.src = "audio/fly.mp3";
+score_audio.src = "audio/score.mp3";
+
 var gap = 90;
 
-//При нажатии на какую-либо кнопку
-document.addEventListener("keydown", moveUp);  // отслеживает какое либо действие
+// При нажатии на какую-либо кнопку
+document.addEventListener("keydown", moveUp);
 
 function moveUp() {
-  yPos -= 20;
+  yPos -= 25;
+  fly.play();
 }
 
-//Создание блоков
+// Создание блоков
 var pipe = [];
 
 pipe[0] = {
@@ -30,27 +38,52 @@ pipe[0] = {
   y : 0
 }
 
-//Позиция птички
+var score = 0;
+// Позиция птички
 var xPos = 10;
 var yPos = 150;
-var grav = 1;
+var grav = 1.5;
 
 function draw() {
-  ctx.drawImage(bg, 0, 0);
+ctx.drawImage(bg, 0, 0);
 
-  //создание цикла
-  for(var i = 0; i < pipe.length; i++) {
-    ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-    ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+for(var i = 0; i < pipe.length; i++) {
+  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+  pipe[i].x--;
 
-    pipe[i].x--;
+  if(pipe[i].x == 125) {
+    pipe.push({
+    x : cvs.width,
+    y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+    });
   }
 
-  ctx.drawImage(fg, 0, cvs.height - fg.height);
-  ctx.drawImage(bird, xPos, yPos);
+ // Отслеживание прикосновений
+  if(xPos + bird.width >= pipe[i].x
+    && xPos <= pipe[i].x + pipeUp.width
+    && (yPos <= pipe[i].y + pipeUp.height
+    || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
+    || yPos + bird.height >= cvs.height - fg.height) {
+      location.reload(); // Перезагрузка страницы
+    }
 
-  yPos += grav;
-  requestAnimationFrame(draw);
+  if(pipe[i].x == 5) {
+    score++;
+    score_audio.play();
+  }
+}
+
+ctx.drawImage(fg, 0, cvs.height - fg.height);
+ctx.drawImage(bird, xPos, yPos);
+
+yPos += grav;
+
+ctx.fillStyle = "#000";
+ctx.font = "18px Verdana";
+ctx.fillText("Score: " + score, 10, cvs.height - 20);
+
+requestAnimationFrame(draw);
 }
 
 pipeBottom.onload = draw;
